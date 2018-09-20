334a0e2ead304426dc5c590eac7d93150ed2ca4a
Rob Owens <rtowens17@gmail.com>
2018-09-19 21:22:32 -0500

    Update Knapsack.cpp
    Made the code compile, now will compile and run.
    Have not thoroughly tested this code though

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <random>
+#include <stdio.h>
 using namespace std;
+
 
 int numberOfBins;
 int binMaxWeight;
@@ -21,17 +20,23 @@ object ** population;
 
 int fitness(object *chromosome)
 {
+	// cout << "Testing Fitness" << endl;
+	
 	int fitnessScore = 0;
-	for (int i = 0; i < sizeof(chromosome); i++)
+	for (int j = 0; j < numberOfObjects; j++)
 	{
-		if (chromosome[i].knapsack != 0)
-			fitnessScore += chromosome[i].value;
+		// cout << "Current Object is " << j << ", in knapsack #";
+		// cout << chromosome[j].knapsack << endl;
+		if (chromosome[j].knapsack != 0)
+			fitnessScore += chromosome[j].value;
 	}
+	
+	// cout << "Fitness is " << fitnessScore << endl;
 
 	return fitnessScore;
 }
 
-// make a new chromosome from chromosome1 and chromosome2 by taking a random number of the objets from each parent chromosome
+// make a new chromosome from chromosome1 and chromosome2 by taking a random number of the objects from each parent chromosome
 object * crossover(object *chromosome1, object *chromosome2)
 {
 	// Make a new chromosome
@@ -52,41 +57,52 @@ object * crossover(object *chromosome1, object *chromosome2)
 	return offspring;
 }
 
-// make a copy of the chromosome but mutate 1 position
-object * mutation(object *chromosome)
-{
-	// Make a new chromosome
-	object * offspring = new object[numberOfObjects];
-
-	for (int i = 0; i < sizeof(chromosome); i++)
-		offspring[i] = chromosome[i];
-
-	// pick a random divide
-	int change = rand() % numberOfObjects;
-	
-	offspring[change] = rand() % numberOfBins;
-
-	return offspring;
-}
-
+// Make a valid chromosome from a potentially invalid chromosome
 object *  fixChromosome(object *chromosome)
 {
 	int *currentWeights = new int[numberOfBins];
+	
+	object * fixedChromosome = new object[numberOfObjects];
 
 	// For each bin in, make sure the chromosome is not over weight
-	for (int i = 0; i < sizeof(chromosome); i++)
+	for (int i = 0; i < numberOfObjects; i++)
 	{
 		// Add the weight to the knapsack if it's in a knapsack
 		currentWeights[chromosome[i].knapsack] += chromosome[i].weight;
+		fixedChromosome[i] = chromosome[i];
 
 		// If a knapsack exceeds capacity
 		if (currentWeights[chromosome[i].knapsack] > binMaxWeight && chromosome[i].knapsack != 0)
 		{
 			// remove the object from the knapsack
 			currentWeights[chromosome[i].knapsack] -= chromosome[i].weight;
-			chromosome[i].knapsack = 0;
+			fixedChromosome[i].knapsack = 0;
 		}
 	}
+	
+	delete chromosome;
+	
+	return fixedChromosome;
+}
+
+// make a copy of the chromosome but mutate 1 position
+object * mutation(object *chromosome)
+{
+	// Make a new chromosome
+	object * offspring = new object[numberOfObjects];
+
+	for (int i = 0; i < numberOfObjects; i++)
+		offspring[i] = chromosome[i];
+
+	// pick a random divide
+	int change = (rand() % numberOfObjects) + 1;
+	
+	offspring[change].knapsack = rand() % numberOfBins;
+	
+	// Make sure the chromosome it valid
+	offspring = fixChromosome(offspring);
+
+	return offspring;
 }
 
 void runGeneration()
@@ -98,25 +114,28 @@ void runGeneration()
 	int newGenPopSize = 0;
 
 	// Do tournament selection to generate a new population
-	for (int i = 0; i < 2 * populationSize - 1; i++)
+	for (int i = 0; i < (2 * populationSize) - 1; i++)
 	{
-		// randomly get challengers
+		// randomly get a challenger
 		int challenger = (rand() % populationSize);
 		int challenger2 = (rand() % populationSize);
-
+		
+		object * chromosome1 = population[challenger];
+		object * chromosome2 = population[challenger2];
+		
 		// if there was a previous winner
 		if (parent != NULL)
 		{
 			// crossover and mutate the child
 			newGeneration[newGenPopSize] = 
-				mutation(crossover(parent, ((population[challenger2] > population[challenger]) ? population[challenger2] : population[challenger])));
+				mutation(crossover(parent, ((fitness(chromosome1) > fitness(chromosome2)) ? chromosome1 : chromosome2)));
 
 			// increase population size and remove previous winner
 			newGenPopSize++;
 			parent = NULL;
 		}
 		else // set the winner as previous winner
-			parent = (population[challenger2] > population[challenger]) ? population[challenger2] : population[challenger];
+			parent = (fitness(chromosome1) > fitness(chromosome2)) ? chromosome1 : chromosome2;
 	}
 
 	// take the best chromosome from last generation
@@ -132,19 +151,33 @@ void runGeneration()
 		}
 	}
 
+	// Output Information about generation
 	newGeneration[newGenPopSize] = population[bestChromosome];
+	cout << "Best Chromosome from Last Generation: " << endl;
+	object * best = population[bestChromosome];
+	for (int i = 0; i < numberOfObjects; i++)
+	{
+		cout << "Knapsack: "<< best[i].knapsack << " Value: "<< best[i].value << " Weight: "<< best[i].weight << endl;
+	}
 	
-	// TODO: display info about generation
+	cout << "Fitness: " << bestFitness << endl;
+	
+	population = newGeneration;
 }
 
-object * generateRandomChromosome()
+// make a new randomized chromosome
+object * generateRandomChromosome(int * value, int * weight)
 {
 	object * chromosome = new object[numberOfObjects];
 
 	for (int i = 0; i < numberOfObjects; i++)
 	{
-		chromosome[i] = rand() % numberOfBins;
+		chromosome[i].knapsack = rand() % numberOfBins;
+		chromosome[i].value = value[i];
+		chromosome[i].weight = weight[i];
+		// cout << chromosome[i].knapsack << ", " << chromosome[i].value << ", " << chromosome[i].weight << " | ";
 	}
+	// cout << endl;
 
 	return chromosome;
 }
@@ -152,18 +185,65 @@ object * generateRandomChromosome()
 
 int main()
 {
+	// Get the instance
+	cout << "Enter a Population Size: ";
+	cin >> populationSize;
+	
+	cout << "Enter number of generations: ";
+	cin >> generations;
+	
+	cout << "Enter the number of objects: ";
+	cin >> numberOfObjects;
+	
+	cout << "Enter the number of knapsacks: ";
+	cin >> numberOfBins;
+	
+	cout << "Enter the maximum weight of a knapsack: ";
+	cin >> binMaxWeight;
+	
+	int seed = 0;
+	
+	srand(seed);
+	
+	int *values = new int[numberOfObjects];
+	int *weights = new int[numberOfObjects];
+	
+	char inputValues;
+	
+	cout << "Input values and weights? (y/n) ";
+	cin >> inputValues;
+	
+
+	for (int i = 0; i < numberOfObjects; i++)
+	{
+		if(inputValues = 'y')
+		{
+			cout << "Enter a value for object " << i << ": ";
+			cin >> values[i];
+			
+			cout << "Enter a weight for object " << i << ": ";
+			cin >> weights[i];
+		}
+		else 
+		{
+			values[i] = rand() % 10;
+			weights[i] = rand() % binMaxWeight;
+		}
+	}
+
 	// generate a random starting population
 	population = new object * [populationSize];
 
 	for (int i = 0; i < populationSize; i++)
 	{
-		population[i] = generateRandomChromosome();
-		population[i] = fixChromosome(population[i]);
+		population[i]  = generateRandomChromosome(values, weights);
+		population[i]  = fixChromosome(population[i]);
 	}
 
 	// run the generations
 	for(int i = 0; i < generations; i++)
 	{
+		cout << "Running generation " << i << endl;
 		runGeneration();
 	}
 
